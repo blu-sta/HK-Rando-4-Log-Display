0b533e7318a01cf3d9b828c01de9c9054700ad18
blu-sta <25652390+blu-sta@users.noreply.github.com>
2023-02-21 17:34:04 +1100

    Allow MW players to start name with numbers

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -6,7 +6,7 @@ namespace HK_Rando_4_Log_Display.Extensions
     public static class Extensions
     {
         public static string AsObjectName(this string s) =>
-            Regex.Replace(s, @"[^\w]", "");
+            "_" + Regex.Replace(s, @"[^\w]", "");
 
         public static string WithoutUnderscores(this string s) =>
             Regex.Replace(s, "_", " ");
